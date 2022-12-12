411d9a040cbd4898cea8a85e81d14d7d8bfae37b
ashwini1966 <bhawalkarashwini1996@gmail.com>
2022-12-12 11:32:53 +0530

    hover issue

--- src/main/career/Role.js
+++ src/main/career/Role.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import {Link} from 'react-router-dom'
 
 function Role() {
-
   const [jobs, setJobs] = useState([]);
  
   async function fetchData() {
@@ -21,6 +20,7 @@ function Role() {
     fetchData();
   },[]);
 
+  
   return (
     <div>
       <section id="opportunities">
@@ -44,15 +44,14 @@ function Role() {
               return (
               <>
                 <div className="col-md-4">
-                  <Link to={`/jobdetails/${job.id}`} >
+                  <Link style={{ "--background-color": `${job.border_color}` }} to={`/jobdetails/${job.id}`} className="link-hover" >
                     <div className="opportunities1" style={{ borderColor : `${job.border_color}`}} >
-                        <div className="opportunities1_body" onMouseOver={({target})=>target.style.backgroundColor=`${job.border_color}`}
-                          onMouseOut={({target})=>target.style.backgroundColor=''} >
-                          <h3 className="text-center">ROLE</h3>
-                          <h2 className="text-center pt-3 pb-3 h2-font" >{job.job_name}</h2>
-                          <h5 className="text-center">Desired Experience {job.experience}</h5>
+                          <div className="opportunities1_body  link-hover">
+                            <h3 className="text-center">ROLE</h3>
+                            <h2 className="text-center pt-3 pb-3 h2-font" >{job.job_name}</h2>
+                            <h5 className="text-center">Desired Experience {job.experience}</h5>
+                        </div>
                       </div>
-                    </div>
                   </Link>
                 </div>
               </>
@@ -60,89 +59,6 @@ function Role() {
             })
           }
           </div>
-
-          {/* <div className="row mt-4 ml-3">
-            <div className="col-md-4">
-              <div className="opportunities1">
-                <div className="opportunities1_body" style={{
-                  backgroundColor: isYellowActive ? "#ffb300" : ''}}>
-                  
-                  <a href="#!" onClick={getRole('Wordpress Developer','#ffb300')} style={{
-                  color: isYellowActive ? 'white' : 'black' }}>
-                    <h3 className="text-center">ROLE</h3>
-                    <h2 className="text-center pt-3 pb-3">Wordpress Developer</h2>
-                    <h5 className="text-center">Desired Experience 2+ Years</h5>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="opportunities2">
-                <div className="opportunities2_body" style={{
-                  backgroundColor: isBlueActive ? "#4385f4" : ''}}>
-                  <a href="#!"  onClick={getRole('Codeigniter Developer','#4385f4')} style={{
-                  color: isBlueActive ? 'white' : 'black' }}>
-                    <h3 className="text-center">ROLE</h3>
-                    <h2 className="text-center pt-3 pb-3">Codeigniter Developer</h2>
-                    <h5 className="text-center">Desired Experience 1+ Years</h5>
-                  </a> 
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="opportunities3">
-                <div className="opportunities3_body" style={{
-                  backgroundColor: isGreenActive ? "#4eacae" : ''}}>
-                  <a href="#!" onClick={getRole('Laravel Developer','#4eacae')} style={{
-                  color: isGreenActive ? 'white' : 'black' }}>
-                    <h3 className="text-center">ROLE</h3>
-                    <h2 className="text-center pt-3 pb-3">Laravel Developer</h2>
-                    <h5 className="text-center">Desired Experience 1+ Years</h5>
-                  </a>  
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="opportunities4">
-                <div className="opportunities4_body" style={{
-                  backgroundColor: isPistaActive ? "#c4df9b" : ''}}>
-                  <a href="#!" onClick={getRole('PHP Developer','#c4df9b')} style={{
-                  color: isPistaActive ? 'white' : 'black' }}>
-                    <h3 className="text-center">ROLE</h3>
-                    <h2 className="text-center pt-3 pb-3">PHP <br/> Developer</h2>
-                    <h5 className="text-center">Desired Experience 1+ Years</h5>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="opportunities5">
-                <div className="opportunities5_body" style={{
-                  backgroundColor: isPinkActive ? "#ffa8ff" : ''}}>
-                  <a href="#!" onClick={getRole('Digital Marketing Executive','#ffa8ff')} style={{
-                  color: isPinkActive ? 'white' : 'black' }}>
-                    <h3 className="text-center">ROLE</h3>
-                    <h2 className="text-center pt-3 pb-3">Digital Marketing Executive</h2>
-                    <h5 className="text-center">Desired Experience 0-1 Years</h5>
-                  </a>  
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="opportunities6">
-                <div className="opportunities6_body" style={{
-                  backgroundColor: isOrangeActive ? "#ff7800" : ''}}>
-                  <a href="#!" onClick={getRole('Business Development Executive','#ff7800')} style={{
-                  color: isOrangeActive ? 'white' : 'black'}}>
-                    <h3 className="text-center">ROLE</h3>
-                    <h2 className="text-center pt-3 pb-3">Business Development Executive</h2>
-                    <h5 className="text-center">Desired Experience 1+ Years</h5>
-                  </a>  
-                </div>
-              </div>
-            </div>
-          </div> */}
-          
         </div>
       </section>
     </div>
